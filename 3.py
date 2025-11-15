49a5ecbed0220a230bc730a7b6ae8bb6bcf7b400
erichuang <erichuangemail@163.com>
2025-11-15 23:30:04 +0800

    6th test

--- 3.py
+++ 3.py
@@ -1,3 +1,7 @@
 this is zt
 
 1 如何从remote repo 更新别人的contribution 到自己的电脑：先update 本地的master 然后merge master into 自己的工作branch
+
+
+
+结论：当我需要贡献代码给远程repo的时候 我需要先从自己的工作branch push代码到remote repo，然后在remote repo那里create pull request，然后让另一个组员approve merge
